c4239070ebda3d3d1c972dbca9822ddfc25eaff0
ankitverma3490 <ankitverma3490@gmail.com>
2024-12-24 12:30:57 +0530

    logo added

--- src/features/auth/SplashScreen.tsx
+++ src/features/auth/SplashScreen.tsx
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
-
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { screenHeight } from '@utils/scaling'
+import logo from '@assets/images/logo.png'
 const SplashScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logoImage}/>
       <Text>SplashScreen</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: Colors.white,
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  logoImage:{
+    width: screenHeight*0.6,
+    height: screenHeight*0.6
+  },
+})
 
 export default SplashScreen
