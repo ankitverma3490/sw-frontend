f3ebdda0812acb0796a103034dc2af1e1dfc3c81
ankitverma3490 <ankitverma3490@gmail.com>
2024-12-24 14:23:55 +0530

    sb updated with sw

--- android/app/src/main/java/com/sw/MainActivity.kt
+++ android/app/src/main/java/com/sw/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sb
+package com.sw
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "sb"
+  override fun getMainComponentName(): String = "sw"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/sw/MainApplication.kt
+++ android/app/src/main/java/com/sw/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.sb
+package com.sw
 
 import android.app.Application
 import com.facebook.react.PackageList
