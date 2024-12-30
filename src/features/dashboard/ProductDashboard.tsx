e7f4d478c1bae07b10192a314c4f8ad5038d74e7
ankitverma3490 <ankitverma3490@gmail.com>
2024-12-31 01:45:14 +0530

    Header completed in dashboard

--- src/features/dashboard/ProductDashboard.tsx
+++ src/features/dashboard/ProductDashboard.tsx
@@ -1,50 +1,80 @@
-import { View, Text,Animated as RNAnimated, SafeAreaView } from 'react-native'
-import React,{useEffect, useRef} from 'react'
- import NoticeAnimation from './NoticeAnimation'
-import { NoticeHeight } from '@utils/scaling'
-import Visuals from './Visuals'
+import {
+  View,
+  Text,
+  Animated as RNAnimated,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import NoticeAnimation from './NoticeAnimation';
+import {NoticeHeight} from '@utils/scaling';
+import Visuals from './Visuals';
+import {
+  CollapsibleContainer,
+  CollapsibleHeaderContainer,
+  withCollapsibleContext,
+} from '@r0b0t3d/react-native-collapsible';
+import AnimatedHeader from './AnimatedHeader';
+import StickSearchBar from './StickSearchBar';
 
+const NOTICE_HEIGHT = -(NoticeHeight + 12);
+const ProductDashboard = () => {
+  const noticePosition = useRef(new RNAnimated.Value(NOTICE_HEIGHT)).current;
+  const slideUp = () => {
+    RNAnimated.timing(noticePosition, {
+      toValue: NOTICE_HEIGHT,
+      duration: 1200,
+      useNativeDriver: false,
+    }).start();
+  };
 
- const NOTICE_HEIGHT = -(NoticeHeight+12)
- const ProductDashboard = () => {
-   
-   const noticePosition =  useRef(new RNAnimated.Value(NOTICE_HEIGHT)).current
-   const slideUp = ()=>{
-    RNAnimated.timing(noticePosition,{
-      toValue:NOTICE_HEIGHT,
-      duration:1200,
-      useNativeDriver:false,
-    }).start()
-   }
+  const slideDown = () => {
+    RNAnimated.timing(noticePosition, {
+      toValue: 0,
+      duration: 1200,
+      useNativeDriver: false,
+    }).start();
+  };
+  useEffect(() => {
+    slideDown();
+    const timeId = setTimeout(() => {
+      slideUp();
+    }, 3500); 
+    return () => {
+      clearTimeout(timeId);
+    };
+  }, []);
 
-   const slideDown = ()=>{
-    RNAnimated.timing(noticePosition,{
-      toValue:0,
-      duration:1200,
-      useNativeDriver:false,
-    }).start()
-   }
-   useEffect(()=>{
-     slideDown()
-     const timeId = setTimeout(()=>{
-      slideUp()
-     }, 3500)
-     return ()=>{clearTimeout(timeId)}
-   },[])
-
-    return (
+  return (
     <NoticeAnimation noticePosition={noticePosition}>
       <>
-      <Visuals/>
-      <SafeAreaView>
-    <View style={{flex:1}}>
-      
-      <Text>ProductDashboard</Text>
-    </View>
-    </SafeAreaView>
-    </>
+        <Visuals />
+         <SafeAreaView/>
+          <CollapsibleContainer style={styles.panelContainer}>
+            <CollapsibleHeaderContainer containerStyle={styles.transparent}>
+              <AnimatedHeader
+                showNotice={() => {
+                  slideDown();
+                  const timeId = setTimeout(() => {
+                    slideUp();
+                  }, 3500);
+                  return () => clearTimeout(timeId);
+                }}
+              />
+              <StickSearchBar />
+            </CollapsibleHeaderContainer>
+          </CollapsibleContainer>
+         
+      </>
     </NoticeAnimation>
-  )
-}
-
-export default ProductDashboard
+  );
+};
+const styles = StyleSheet.create({
+  panelContainer: {
+    flex: 1,
+  },
+  transparent: {
+    backgroundColor: 'transparent',
+  },
+});
+export default withCollapsibleContext(ProductDashboard);
--- src/features/dashboard/Visuals.tsx
+++ src/features/dashboard/Visuals.tsx
@@ -1,12 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet, Image} from 'react-native';
+import React, {FC} from 'react';
+import Animated from 'react-native-reanimated';
+import LinearGradient from 'react-native-linear-gradient';
+import {darkWeatherColors} from '@utils/Constants';
+import {screenHeight, screenWidth} from '@utils/scaling';
+import LottieView from 'lottie-react-native';
 
-const Visuals = () => {
+const Visuals: FC = () => {
   return (
-    <View>
-      <Text>Visuals</Text>
-    </View>
-  )
-}
+    <Animated.View style={styles.container}>
+      <LinearGradient colors={darkWeatherColors} style={styles.gradient} />
+      <Image source={require('@assets/images/cloud.png')} />
+      <LottieView
+        autoPlay={true}
+        enableMergePathsAndroidForKitKatAndAbove={true}
+        loop={true}
+        style={styles.lottie}
+        source={require('@assets/animations/raining.json')}
+      />
+    </Animated.View>
+  );
+};
 
-export default Visuals
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+  },
+  lottie: {
+    width: '100%',
+    height: 150,
+    position: 'absolute',
+    transform: [{scaleX: -1}],
+  },
+  gradient: {
+    width: '100%',
+    height: screenHeight * 0.4,
+    position: 'absolute',
+  },
+  cloud: {
+    width: screenWidth,
+    resizeMode: 'stretch',
+    height: 100,
+  },
+});
+export default Visuals;
